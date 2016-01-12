caa11adb21c771b62fc2bdd7508e46471d83f8b2
KrazeeKoder <tony.tulai@gmail.com>
2016-01-12 13:55:32 -0500

    Blank Project

---
+++ ContactList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Anthony Tulai on 2016-01-12.
+//  Copyright © 2016 Anthony Tulai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
